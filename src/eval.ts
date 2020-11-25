223ffe0d5eca93f6e589dc005a57697168a29ca1
Clay Danford <clay.danford@candidpartners.com>
2020-11-25 16:35:55 -0600

    ddb direct write

--- src/eval.ts
+++ src/eval.ts
@@ -1,9 +1,11 @@
 import 'source-map-support/register'
+import cryptoRandomString from 'crypto-random-string'
 
 export const handler = async (event: any) => {
   const cc = event['Credit Card']
 
   const response = {
+    id: cryptoRandomString({ length: 20 }),
     statusCode: 200,
     fraudulant: false,
     ...event,
